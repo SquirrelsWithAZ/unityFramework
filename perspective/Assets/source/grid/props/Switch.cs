db6739bde7114f1d937af5e9da873bda3042d1e3
FutureJames <james415@gmail.com>
2014-01-26 12:21:07 -0800

    switch changes

--- perspective/Assets/source/grid/props/Switch.cs
+++ perspective/Assets/source/grid/props/Switch.cs
@@ -4,8 +4,16 @@ using System.Collections;
 public class Switch : Prop 
 {
 	public double cooldown;
+  public Renderer switchRenderer;
+  public AnimationCurve cooldownColorCurve;
 
+  private Color _initialColor;
   private double _cooldownCounter;
+
+  public void Start()
+  {
+    _initialColor = switchRenderer.material.color;
+  }
 
   public void StepOnSwitch()
   {
@@ -14,6 +22,8 @@ public class Switch : Prop
 
       //Game.instance.grid.swapTileState();
       moveSphereCollider();
+      Game.instance.grid.swapTileState();
+      Game.instance.grid.swapTileVisuals();
       _cooldownCounter = cooldown;
     }
   }
@@ -21,9 +31,15 @@ public class Switch : Prop
   private void Update()
   {
     if (_cooldownCounter > 0)
+    {
+      switchRenderer.material.color = Color.Lerp( _initialColor, Color.gray,cooldownColorCurve.Evaluate((float)(_cooldownCounter / cooldown)));
       _cooldownCounter -= Time.deltaTime;
+    }
     else if (_cooldownCounter < 0)
+    {
+      switchRenderer.material.color = _initialColor;
       _cooldownCounter = 0;
+    }
   }
 
   //snap the sphere collider over and trigger to kick off the state change 

3457a049d66de923e7edf79f04a1d2650d0fa618
Aaron Biddlecom <aaronbiddlecom@gmail.com>
2014-01-26 11:24:14 -0800

    Put color cooldown on Switch

--- perspective/Assets/source/grid/props/Switch.cs
+++ perspective/Assets/source/grid/props/Switch.cs
@@ -4,8 +4,16 @@ using System.Collections;
 public class Switch : Prop 
 {
 	public double cooldown;
+  public Renderer switchRenderer;
+  public AnimationCurve cooldownColorCurve;
 
+  private Color _initialColor;
   private double _cooldownCounter;
+
+  public void Start()
+  {
+    _initialColor = switchRenderer.material.color;
+  }
 
   public void StepOnSwitch()
   {
@@ -20,8 +28,14 @@ public class Switch : Prop
   private void Update()
   {
     if (_cooldownCounter > 0)
+    {
+      switchRenderer.material.color = Color.Lerp( _initialColor, Color.gray,cooldownColorCurve.Evaluate((float)(_cooldownCounter / cooldown)));
       _cooldownCounter -= Time.deltaTime;
+    }
     else if (_cooldownCounter < 0)
+    {
+      switchRenderer.material.color = _initialColor;
       _cooldownCounter = 0;
+    }
   }
 }

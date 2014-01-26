a1e3731954ad124ec505c76e329a4fae2ea20f42
Aaron Biddlecom <aaronbiddlecom@gmail.com>
2014-01-25 18:18:52 -0800

    FIXED -- DON'T ADD Assets/assets!!!!

---
+++ perspective/Assets/source/TestController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestController : MonoBehaviour 
+{
+  public PlayerInputManager inputManager;
+	// Use this for initialization
+	void Start () 
+  {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+  {
+    if (inputManager.CheckInput(InputActions.Down).isActive)
+      transform.position -= new Vector3(0f, 0f, -5f) * Time.deltaTime;
+	}
+}

1cc959b7457761d31985d3320460550b4bf1b002
Aaron Biddlecom <aaronbiddlecom@gmail.com>
2014-01-26 15:56:50 -0800

    Anims in

---
+++ perspective/Assets/source/AvatarAnimationManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AvatarAnimationManager : MonoBehaviour {
+  public Avatar avatar;
+  public Animation animationRoot;
+  public AnimationClip idleClip;
+  public AnimationClip idleToRunClip;
+  public AnimationClip runClip;
+
+  private bool _running;
+
+  public void Start()
+  {
+    animationRoot.PlayQueued(idleClip.name, QueueMode.PlayNow);  
+  }
+  public void Update()
+  {
+    if(avatar._currentVelocity.sqrMagnitude != 0 && !_running)
+    {
+      _running = true;
+      animationRoot.PlayQueued(idleToRunClip.name, QueueMode.PlayNow);
+      animationRoot.PlayQueued(runClip.name, QueueMode.CompleteOthers);
+    }
+    else if(avatar._currentVelocity.sqrMagnitude == 0 && _running)
+    {
+      _running = false;
+      animationRoot.PlayQueued(idleClip.name, QueueMode.PlayNow);
+    }
+  }
+}

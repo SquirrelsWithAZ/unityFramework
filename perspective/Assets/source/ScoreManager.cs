71411d54802b0b5ef2a862fa7048b17217ae414c
Aaron Biddlecom <aaronbiddlecom@gmail.com>
2014-01-26 13:06:46 -0800

    Additional Score infrastructure

---
+++ perspective/Assets/source/ScoreManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour 
+{
+  public TextMesh playerAScoreText;
+  public TextMesh playerBScoreText;
+
+  private int _playerAScore;
+  private int _playerBScore;
+
+  public void AdjustPlayerScore(TileTypes playerType, int scoreAdjustment)
+  {
+    if (playerType == TileTypes.TypeA)
+    {
+      _playerAScore += scoreAdjustment;
+      playerAScoreText.text = _playerAScore.ToString();
+    }
+    else if (playerType == TileTypes.TypeB)
+    {
+      _playerBScore += scoreAdjustment;
+      playerBScoreText.text = _playerBScore.ToString();
+    }
+  }
+}

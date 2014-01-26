d61a6f04c3a7139a745b3688b30b789b049cd17d
Aaron Biddlecom <aaronbiddlecom@gmail.com>
2014-01-26 14:06:44 -0800

    Change switches to be global

--- perspective/Assets/source/grid/props/Switch.cs
+++ perspective/Assets/source/grid/props/Switch.cs
@@ -1,14 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Switch : Prop 
+public class Switch : Prop
 {
-	public double cooldown;
+  public double cooldown;
   public Renderer switchRenderer;
   public AnimationCurve cooldownColorCurve;
 
   private Color _initialColor;
-  private double _cooldownCounter;
+  private static double _globablCooldown;
+  private static double _cooldownCounter;
 
   public void Start()
   {
@@ -25,7 +26,8 @@ public class Switch : Prop
       moveSphereCollider();
       //Game.instance.grid.swapTileState();
       //Game.instance.grid.swapTileVisuals();
-      _cooldownCounter = cooldown;
+      StartCoroutine(CooldownCounterUpdate());
+      //_cooldownCounter = cooldown;
     }
   }
 
@@ -33,28 +35,43 @@ public class Switch : Prop
   {
     if (_cooldownCounter > 0)
     {
-      switchRenderer.material.color = Color.Lerp( _initialColor, Color.gray,cooldownColorCurve.Evaluate((float)(_cooldownCounter / cooldown)));
-      _cooldownCounter -= Time.deltaTime;
+      switchRenderer.material.color = Color.Lerp(_initialColor, Color.gray, cooldownColorCurve.Evaluate((float)(_cooldownCounter / cooldown)));
     }
     else if (_cooldownCounter < 0)
     {
       switchRenderer.material.color = _initialColor;
-      _cooldownCounter = 0;
     }
   }
 
   //snap the sphere collider over and trigger to kick off the state change 
   private void moveSphereCollider()
   {
-      //Debug.Log("move sphere collider");
+    //Debug.Log("move sphere collider");
 
-      int i = this.transform.GetComponent<Switch>().i;
-      int j = this.transform.GetComponent<Switch>().j;
+    int i = this.transform.GetComponent<Switch>().i;
+    int j = this.transform.GetComponent<Switch>().j;
 
-      //Debug.Log("switch is at: " + i + " , " + j);
+    //Debug.Log("switch is at: " + i + " , " + j);
 
-      GameObject sphere = GameObject.Find("SphereCollider");
-      sphere.GetComponent<SphereColliderAnimation>().Trigger(i*3, j*3);
+    GameObject sphere = GameObject.Find("SphereCollider");
+    SphereColliderAnimation animationManager = sphere.GetComponent<SphereColliderAnimation>();
+    animationManager.Trigger(i * 3, j * 3);
+    _cooldownCounter = animationManager.animation.clip.length;
+    _globablCooldown = _cooldownCounter;
   }
 
+  IEnumerator CooldownCounterUpdate()
+  {
+    while (_cooldownCounter > 0)
+    {
+      _cooldownCounter -= Time.deltaTime;
+      yield return null;
+    }
+
+    if (_cooldownCounter < 0)
+    {
+      yield return null;
+      _cooldownCounter = 0;
+    }
+  }
 }
